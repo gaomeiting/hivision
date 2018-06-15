611e4da2e2726082c0500b869e5b706fe70779dc
gaomeiting <1959151877@qq.com>
2018-06-15 12:04:21 +0000

    删除 api.js

--- api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-export function Post(url, params) {
-	return axios.post(url, params).then(res => {
-		return Promise.resolve(res.data)
-	}).catch(err => {
-		return Promise.reject(err)
-	})
-}
